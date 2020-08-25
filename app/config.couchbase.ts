caa9d18308635311fd4c5fd3503cbc2850e9bbf0
RAJESH-GN <rajeshgn821@gmail.com>
2020-08-25 08:34:43 +0200

    changes to Node application

--- app/config.couchbase.ts
+++ app/config.couchbase.ts
@@ -4,7 +4,7 @@ const cluster = new couchbase.Cluster("couchbase://localhost", {
   username: "Administrator",
   password: "March18@1993",
 });
-const bucket = cluster.bucket("person-sample");
+const bucket = cluster.bucket("couchSample");
 const collection = bucket.defaultCollection();
 
 module.exports = collection;
--- app/controllers/welcome.controller.ts
+++ app/controllers/welcome.controller.ts
@@ -1,50 +1,38 @@
-// Import only what we need from express
 import { Router, Request, Response } from "express";
 const db = require("./../config.couchbase");
-// Assign router to the express.Router() instance
 const router: Router = Router();
 
-// The / here corresponds to the route that the WelcomeController
-// is mounted on in the server.ts file.
-// In this case it's /welcome
-router.get("/", async (req: Request, res: Response) => {
-  // Reply with a hello world when no name param is provided
-  const airline = {
-    type: "sample",
-    id: 8091,
-    callsign: "CBS",
-    iata: null,
-    icao: null,
-    name: "Couchbase",
-  };
-
-  const upsertDocument = async (doc: {
-    type: any;
-    id: any;
-    callsign?: string;
-    iata?: null;
-    icao?: null;
-    name?: string;
-  }) => {
-    try {
-      // key will equal: "airline_8091"
-      const key = `${doc.type}_${doc.id}`;
-      const result = await db.upsert(key, doc);
-      console.log("Upsert Result: ");
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  await upsertDocument(airline);
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await db.get(req.params.id);
+    res.send(result);
+  } catch (error) {
+    console.log("error", error);
+  }
   res.send("Hello, World!");
 });
+
 router.get("/:name", (req: Request, res: Response) => {
-  // Extract the name from the request parameters
   let { name } = req.params;
-  // Greet the given name
   res.send(`Hello, ${name}`);
 });
-// Export the express.Router() instance to be used by server.ts
+
+router.post("/postuser", async (req: Request, res: Response) => {
+  const doc = {
+    type: "user",
+    name: req.body.name,
+    id: getRandomInt(10),
+  };
+  try {
+    const key = `${doc.type}_${doc.id}`;
+    await db.upsert(key, doc);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 export const WelcomeController: Router = router;
